7cd71f1ec2defdf031eaf67a309670b24df059dc
eexpress <eexpress@163.com>
2021-12-29 21:56:42 +0800

    保存menu阵列失败

--- extension.js
+++ extension.js
@@ -6,15 +6,20 @@
 //~ const Mainloop = imports.mainloop;
 //~ Mainloop.timeout_add(3000, function () { text.destroy(); });
 
+imports.gi.versions.Gtk = '3.0';	//GLib need version.
+
 const GETTEXT_DOMAIN = 'countdown-indicator-extension';
 const _ = imports.gettext.domain(GETTEXT_DOMAIN).gettext;
 
-const { GObject, Gio, Clutter, St } = imports.gi;
+const { GObject, GLib, Gio, Clutter, St } = imports.gi;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
 const msg = Main.notify;
+
+	var list = new Array();
+	let sourceId = null;
 
 const Indicator = GObject.registerClass(
 class Indicator extends PanelMenu.Button {
@@ -22,7 +27,7 @@ class Indicator extends PanelMenu.Button {
 		var that = this;	// 想缓存，在闭包中，代替调用this。
 		super._init(0.0, _('Countdown Indicator'));
 //~ -------------------  面板主图标 ---------------------------
-		var stock_icon = new St.Icon({ icon_name: 'alarm-symbolic', icon_size: 36 });
+		var stock_icon = new St.Icon({ icon_name: 'alarm-symbolic', icon_size: 30 });
 		this.add_child(stock_icon);
 //~ ----------------  第一行可选图标组 -------------------------
 		let item_icons = new PopupMenu.PopupMenuItem('');
@@ -60,11 +65,14 @@ class Indicator extends PanelMenu.Button {
 			track_hover: true,
 			x_expand: true,
 		});
-		//~ input.clutter_text.set_input_purpose(Clutter.DIGITS|Clutter.DATETIME);
 		input.connect('primary-icon-clicked', ()=>{add_timer();});
 		input.connect('secondary-icon-clicked', ()=>{add_timer();});
-		//~ input.clutter_text.connect('activate', (actor) => { log(`Activated: ${actor.text}`); });
 		input.clutter_text.connect('activate', (actor) => { add_timer(); });
+		//~ input.connect('primary-icon-clicked', add_timer());
+		//~ input.connect('secondary-icon-clicked', add_timer());
+		//~ input.clutter_text.connect('activate', add_timer());
+		item_input.add(input);
+		this.menu.addMenuItem(item_input);
 		//~ input.connect('key-press-event', (self, event)=>{
 			//~ let [success, keyval] = event.get_keyval();
 			//~ let keyname = Gdk.keyval_name(keyval);
@@ -74,34 +82,41 @@ class Indicator extends PanelMenu.Button {
 			//~ if (event.get_key_symbol() === Clutter.KEY_Enter){add_timer();}
 			//~ if(event.keyval == Clutter.KEY_Enter){add_timer();} Clutter.KEY_Escape KEY_ISO_Enter KEY_KP_Enter KEY_3270_Enter KEY_equal
 		//~ });
-		item_input.add(input);
+//~ ---------------------------------------------------------
 		function add_timer (){
+			//~ return function(){
 			let d = parseInt(input.text);
 			if(isNaN(d) || d < 1){return;}
-			let text = _('  倒计时还剩余 xxxx 分钟，目标：') + d;
+			let text = _('  倒计时还剩余 ') + d + _(' 分钟，目标：') + d;
 			//~ let text = _('  倒计时还剩余 xxxx 分钟，目标：') + input.text;
+			PopupMenu.PopupImageMenuItem.prototype.count = d;
+			PopupMenu.PopupImageMenuItem.prototype.left = d;
 			let item = new PopupMenu.PopupImageMenuItem(text, stock_icon.icon_name);
 			// 无法判断并提取gicon了。只能使用icon_name的stock图标？
 			item.style_class = 'large_text';
 			item.can_focus = true;
-			item.connect('button-press-event', ()=> {delete_item(item);});
-			//~ item.prototype.START = 0;
-			//~ item.prototype.END = d;
-			//~ item.text += item.START +"-x-"+ item.END;
+			item.connect('activate', ()=> {delete_item(item);});
+			input.text = '';
 			that.menu.addMenuItem(item);
+			//~ log(item.left);
+			list.push(item);
+				var i = item;	log(i.count+' <====== '+i.left);
+			//~ }
 		}
 		function delete_item(item){
 			//~ const GLib = imports.gi.GLib;
 			//~ let stuff = GLib.spawn_command_line_sync("zenity --question xxx; echo $?")[1].toString();	//能卡死shell
 			//~ log(stuff);
+			list.forEach((i,index,arr)=>{
+				if(i === item){arr.splice(index,1);}
+			})
 			item.destroy();
 			// clipboard-indicator 自己带的一个 confirmDialog.js
 		}
-		this.menu.addMenuItem(item_input);
 //~ -------------------- 分割栏以下为定时列表 -------------------
 		this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-		//~ let item_sign = new PopupMenu.PopupMenuItem("𝕖𝕖𝕩𝕡𝕤𝕤@𝕘𝕞𝕒𝕚𝕝.𝕔𝕠𝕞");
-		//~ this.menu.addMenuItem(item_sign);
+		//~ let item_sign = new PopupMenu.PopupMenuItem("𝕖𝕖𝕩𝕡𝕤𝕤@𝕘𝕞𝕒𝕚𝕝.𝕔𝕠𝕞"); this.menu.addMenuItem(item_sign);
+//~ ---------------------------------------------------------
 //~ ---------------------------------------------------------
 	}
 });
@@ -122,11 +137,23 @@ class Extension {
 	enable() {
 		this._indicator = new Indicator();
 		Main.panel.addToStatusArea(this._uuid, this._indicator);
+		sourceId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 5, () => {
+			log("======"+list.length+"======");
+			list.forEach((i)=>{
+				log(i.text +': '+ i.count+' <--- '+i.left);
+			})
+			return GLib.SOURCE_CONTINUE;	//true
+			//~ return GLib.SOURCE_REMOVE;		//false
+		});
 	}
 
 	disable() {
 		this._indicator.destroy();
 		this._indicator = null;
+		if (sourceId) {
+			GLib.Source.remove(sourceId);
+			sourceId = null;
+		}
 	}
 }
 
